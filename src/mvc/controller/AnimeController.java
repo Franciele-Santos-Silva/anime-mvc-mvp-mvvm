047ba2b6a36bd4608000b8f0179ac3324e8c04b1
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-04-28 13:33:11 -0300

    fix: corrigir erros de compilação no Controller

--- src/mvc/controller/AnimeController.java
+++ src/mvc/controller/AnimeController.java
@@ -3,19 +3,22 @@ package mvc.controller;
 import mvc.model.Anime;
 import mvc.view.AnimeView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimeController {
 
     private List<Anime> lista = new ArrayList<>();
-    private Anime view;
+    private AnimeView view;
 
     public AnimeController(AnimeView view){
         this.view = view;
     }
 
-    public void adicionarAnimes(String nome, String genero, double nota){
+    public void adicionarAnime(String nome, String genero, double nota){
         Anime anime = new Anime(nome, genero, nota);
         lista .add(anime);
-        view.mostrarMensagem("Anime adicionado com sucesso.")
+        view.mostrarMensagem("Anime adicionado com sucesso.");
     }
 
     public void listarAnimes(){
