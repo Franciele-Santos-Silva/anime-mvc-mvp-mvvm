5fa63c7c76fc7a507e96466f07fb4b48a53b3215
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-04-28 12:57:17 -0300

    feat: criar menu interativo com Scanner

---
+++ mvc/Main.java
@@ -0,0 +1,50 @@
+package mvc;
+
+import mvc.controller.AnimeController;
+import mvc.view.AnimeView;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        AnimeView = new AnimeView();
+        AnimeController controller = new AnimeController(view);
+
+        int opcao;
+
+        do{
+            view.mostrarMenu();
+            opcao = sc.nextInt();
+
+            switch (opcao){
+
+                case 1:
+                    System.out.print("Nome: ");
+                    String nome = sc.nextLine();
+
+                    System.out.print("Gênero: ");
+                    String genero = sc.nextLine();
+
+                    System.out.print("Nota: ");
+                    double nota = sc.nextDouble();
+
+                    controller.adicionarAnime(nome, genero, nota);
+                    break;
+
+                case 2:
+                    controller.listarAnimes();
+                    break;
+
+                case 0:
+                    view.mostrarMensagem("Saindo...");
+                    break;
+
+                default:
+                    view.mostrarMensagem("Opção inválida")
+            }
+        } while (opcao != 0);
+
+        sc.close();
+    }
+}
