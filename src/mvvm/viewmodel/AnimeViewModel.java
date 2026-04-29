8696ff6595071030c070f12b47dc0a48a662b2e5
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-04-29 16:46:32 -0300

    feat: implementar lógica no AnimeViewModel

--- src/mvvm/viewmodel/AnimeViewModel.java
+++ src/mvvm/viewmodel/AnimeViewModel.java
@@ -1,6 +1,20 @@
 package mvvm.viewmodel;
 
-public class AnimeViewModel {
-    
+import mvvm.model.Anime;
 
+import java.util.List;
+import java.util.ArrayList;
+
+public class AnimeViewModel {
+
+    private List<Anime> lista = new ArrayList<>();
+
+    public void adicionarAnime(String nome, String genero, double nota){
+        Anime anime = new Anime(nome, genero, nota);
+        lista.add(anime);
+    }
+
+    public List<Anime> getLista(){
+        return lista;
+    }
 }
