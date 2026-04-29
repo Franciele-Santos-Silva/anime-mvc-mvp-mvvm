c64454b20f7cd9a4cf00df2c90ecec32f8c7a93c
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-04-29 00:38:50 -0300

    feat: adiciona model Anime para representar dados de anime da aquitetura mvp

---
+++ src/mvp/model/Anime.java
@@ -0,0 +1,29 @@
+package mvp.model;
+
+public class Anime {
+    private String nome;
+    private String genero;
+    private double nota;
+
+    public Anime(String nome, String genero, double nota) {
+        this.nome = nome;
+        this.genero = genero;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public String toString(){
+        return "Nome: " + nome + " | Gênero: " + genero + " | Nota: " + nota;
+    }
+}
